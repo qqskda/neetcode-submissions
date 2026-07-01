a5e4e3f28ee5ebbe907c0e064d55e89263772ecd
qqskda <lee.sk.qq@gmail.com>
2026-07-02 00:25:42 +1000

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        // dance
+        for (int i = 0; i < prefix.size(); ++i)
+        {
+          // dance
+          char ch = prefix[i];
+          for (int j = 1; j < strs.size(); ++j)
+          {
+            // dag danger damage
+            if (i >= strs[j].size() || ch != strs[j][i])
+            {
+              // prefix mismatch!
+              return prefix.substr(0, i);
+            }
+          }
+        }
+        return prefix;
+    }
+};
