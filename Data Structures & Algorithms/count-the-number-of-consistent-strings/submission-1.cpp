6910c5a4348dab046acf411580dee34212f86063
qqskda <lee.sk.qq@gmail.com>
2026-07-22 20:09:00 +1000

    Add: count-the-number-of-consistent-strings - submission-1

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int ret = 0;
+        // std::array<int, 27> arr = {0};
+        int arr[27] = {};
+
+        for (const char ch : allowed)
+        {
+          arr[ch-'a'] = 1;
+        }
+        for (const string& word : words)
+        {
+          ++ret;
+          for (const char ch : word)
+          {
+            if (arr[ch-'a'] == 0)
+            {
+              --ret;
+              break;
+            }
+          }
+        }
+        return ret;
+    }
+};
