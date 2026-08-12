707826d8ab0f9fd83f0683e9562c7d5f11aeec43
qqskda <lee.sk.qq@gmail.com>
2026-08-12 19:53:15 +1000

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int dpf(const vector<int>& coins, vector<int>& dp, int amount)
+    {
+        if (amount < 0) return -1; // not possible
+
+        if (dp[amount] != -2)
+        {
+            // we have been here
+            return dp[amount];
+        }
+
+        int bestMin = INT_MAX;
+        for (const int& coin : coins)
+        {
+            int output = dpf(coins, dp, amount - coin);
+            if (output >= 0)
+            {
+                bestMin = min(bestMin, output);
+            }
+        }
+        dp[amount] = (bestMin == INT_MAX) ? -1 : bestMin + 1;
+        return dp[amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        // amount 12, 1 5 10
+        // minCoins (12) = min(minCoins(11), minCoins(7), minCoins(2)) + 1;
+        // ... minCoins(1) = min(minCoins(0), minCoins(-4), minCoins(-9)) + 1;
+
+        vector<int> dp(amount+1,-2);
+        dp[0] = 0;
+        return dpf(coins, dp, amount);
+    }
+};
