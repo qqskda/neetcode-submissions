199e668197143aa7decdc8d948a3de77f1fcd82b
qqskda <lee.sk.qq@gmail.com>
2026-08-18 16:37:16 +1000

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // basically same as binary search using the first element of each row
+        // like left, mid, right
+
+        // once you find the row that nums[mid][0] < target && nums[right][0] > target
+        // Check the row if it has the taret
+
+        int left = 0, right = matrix.size()-1;
+        while (left < right) // Converge binary search making left==right after loop
+        {
+            int mid = left + ((right-left+1)/2); // left = mid;
+            // int mid = left + ((right-left)/2); // right = mid;
+            
+            // early return
+            if (matrix[mid][0] == target) return true;
+
+            if (matrix[mid][0] > target)
+                right = mid-1;
+            else // matrix[mid][0] <= target 
+                left = mid;
+        }
+        // left == right
+
+        // we now know the row
+        int row = left;
+        left = 0; right = matrix.front().size()-1;
+
+        if (matrix[row][right] < target) return false;
+
+        while (left <= right) // answer binary search
+        {
+            int mid = left + ((right-left)/2);
+
+            if (matrix[row][mid] == target) return true;
+            
+            if (matrix[row][mid] < target)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return false;
+    }
+};
