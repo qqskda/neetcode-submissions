23b2e5d981e1277ee171e4929d51312ce1a9f95d
qqskda <lee.sk.qq@gmail.com>
2026-08-24 23:55:46 +1000

    Add: can-place-flowers - submission-3

---
+++ Data Structures & Algorithms/can-place-flowers/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        // canPlant[i] = empty[i-1] && empty[i+1];
+        if (n == 0) return true;
+        int count = 0;
+
+        int sz = flowerbed.size();
+        bool left = false, right = false;
+        for (int i = 0; i < sz; ++i)
+        {
+            // This spot is already taken, skip
+            if (flowerbed[i] == 1) continue;
+
+            // Setup for the boundaries
+            if (i == 0) left = true;
+            else left = flowerbed[i-1] == 0;
+            if (i == sz-1) right = true;
+            else right = flowerbed[i+1] == 0;
+
+            if (left && right) // neighbours are empty
+            {
+                flowerbed[i] = 1;
+                ++count;
+            }
+            if (count >= n) return true; //early exit
+        }
+        return false;
+    }
+};
