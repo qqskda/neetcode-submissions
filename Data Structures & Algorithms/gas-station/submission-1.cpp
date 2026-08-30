a831007d454dfd4da8c65bc04063f3363fc79b03
qqskda <lee.sk.qq@gmail.com>
2026-08-30 17:15:48 +1000

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        // 1. if the sum of cost is higher than gas, it is impossible.
+        int total_gas = accumulate(gas.begin(), gas.end(), 0);
+        int total_cost = accumulate(cost.begin(), cost.end(), 0);
+        if (total_gas < total_cost) return -1;
+
+        int start = 0;
+        int curr_gas = 0;
+        for (int i = 0; i < gas.size(); ++i)
+        {
+            curr_gas += gas[i];
+
+            if (cost[i] > curr_gas)
+            {
+                // dont have enough gas. Reset starting point
+                start = i+1;
+                curr_gas = 0;
+                continue;
+            }
+            // we do have enough gas
+            curr_gas -= cost[i];
+        }
+
+        return start;
+    }
+};
