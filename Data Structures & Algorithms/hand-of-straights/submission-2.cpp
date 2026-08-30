078c929ef38cf5b3f3d047ecadffe3a0cdd0d4c4
qqskda <lee.sk.qq@gmail.com>
2026-08-31 00:19:43 +1000

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        // Q: Use all the card we have to form group/s of groupSize.
+
+        map<int,int> mp; // key-freq
+
+        for (int i = 0; i < hand.size(); ++i)
+        {
+            mp[hand[i]] += 1;
+        }
+
+        // we have to use all the cards so start from the first
+        auto itr = mp.begin();
+        for (auto itr = mp.begin(); itr != mp.end(); )
+        {
+            if (itr->second == 0)
+            {
+                itr++;
+                continue;
+            }
+
+            int start = itr->first;
+            itr->second--;
+
+            for (int i = 1; i < groupSize; ++i)
+            {
+                int next = start+1;
+                int next_freq = mp[next];
+                if (!mp.contains(next)) return false;
+                if (mp[next] == 0) return false;
+                
+                mp[next]--;
+                start = next;
+            }
+        }
+        return true;
+
+    }
+};
